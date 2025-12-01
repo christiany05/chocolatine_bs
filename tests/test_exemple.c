7fb421a490f6db7221fbb3b02b13fd0fccca9096
Ton Nom <ton.email@example.com>
2025-12-01 17:18:37 +0100

    test

--- tests/test_exemple.c
+++ tests/test_exemple.c
@@ -9,6 +9,7 @@
 #include <criterion/redirect.h>
 #include <unistd.h>
 #include <sys/wait.h>
+#include <stdio.h>
 #include "../panoramix.h"
 
 char *run_panoramix(int nb_villagers, int pot_size, int nb_fights, int nb_refills)
@@ -49,14 +50,37 @@ Test(panoramix_suite, basic_scenario_complete)
 
 int run_panoramix_and_get_exit_code(char *arg1, char *arg2, char *arg3, char *arg4)
 {
-    return 84; 
+    pid_t pid;
+    int status;
+    char *argv[] = {"./panoramix", arg1, arg2, arg3, arg4, NULL};
+    
+    pid = fork();
+    
+    if (pid == -1) {
+        perror("fork");
+        return -1;
+    } else if (pid == 0) {
+        execvp(argv[0], argv); 
+        fprintf(stderr, "Erreur d'exécution de ./panoramix\n");
+        _exit(1);
+    } else {
+        if (waitpid(pid, &status, 0) == -1) {
+            perror("waitpid");
+            return -1;
+        }
+        if (WIFEXITED(status)) {
+            return WEXITSTATUS(status);
+        }
+    }
+    return -1;
 }
 
 Test(panoramix_suite, invalid_nb_villagers)
 {
     int exit_code;
-
-    cr_redirect_stdout(); 
+    
     exit_code = run_panoramix_and_get_exit_code("0", "5", "3", "1");
-    cr_assert_neq(exit_code, 0, "Le programme doit échouer (exit code non nul) pour nb_villagers <= 0.");
+    
+    cr_assert_neq(exit_code, 0, "Le programme doit échouer pour nb_villagers=0.");
+    
 }
